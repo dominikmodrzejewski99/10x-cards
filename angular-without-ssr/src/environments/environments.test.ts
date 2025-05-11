bc9dc7c448234f52de105627c8ccf7b4d3f8f901
Dominik Modrzejewski <dominik.modrzejewski@wfirma.pl>
2025-05-11 20:33:21 +0200

    chore: update e2e tests

---
+++ angular-without-ssr/src/environments/environments.test.ts
@@ -0,0 +1,12 @@
+// Plik środowiskowy dla testów E2E
+// Nie używa obiektu window, który nie jest dostępny w środowisku Node.js
+
+export const environment = {
+  production: false,
+  supabaseUrl: 'http://127.0.0.1:54321',
+  supabaseKey: '',
+  openRouterKey: '',
+  E2E_USERNAME_ID: '054cdcec-17dc-47f2-8979-915a23ff1d7a',
+  E2E_USERNAME: 'test@gmail.com',
+  E2E_PASSWORD: '123123qwe'
+};
