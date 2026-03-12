98a2d3d671d399662aa24350ab9055c17383de3d
dominikmodrzejewski99 <dominikmodrzejewski@wp.pl>
2026-03-12 18:59:47 +0100

    feat: add ReviewReminderService for session-scoped due card checks

---
+++ angular-without-ssr/src/app/shared/services/review-reminder.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, inject } from '@angular/core';
+import { Observable, of, map } from 'rxjs';
+import { ReviewApiService } from '../../services/review-api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReviewReminderService {
+  private reviewApi = inject(ReviewApiService);
+  private shownThisSession = false;
+
+  checkDueCards(): Observable<number> {
+    if (this.shownThisSession) {
+      return of(0);
+    }
+
+    return this.reviewApi.getDueCards().pipe(
+      map(cards => cards.length)
+    );
+  }
+
+  markAsShown(): void {
+    this.shownThisSession = true;
+  }
+}
