5095e9b9931b59357ddeceb1de7fafd1bcb4310e
dominikmodrzejewski99 <dominikmodrzejewski@wp.pl>
2026-03-12 19:00:51 +0100

    feat: add ReviewReminderComponent (dumb/presentational)

---
+++ angular-without-ssr/src/app/shared/components/review-reminder/review-reminder.component.ts
@@ -0,0 +1,25 @@
+import { Component, ChangeDetectionStrategy, input, output } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+
+@Component({
+  selector: 'app-review-reminder',
+  imports: [DialogModule],
+  templateUrl: './review-reminder.component.html',
+  styleUrls: ['./review-reminder.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ReviewReminderComponent {
+  visible = input.required<boolean>();
+  dueCount = input.required<number>();
+
+  study = output<void>();
+  dismiss = output<void>();
+
+  onStudy(): void {
+    this.study.emit();
+  }
+
+  onDismiss(): void {
+    this.dismiss.emit();
+  }
+}
